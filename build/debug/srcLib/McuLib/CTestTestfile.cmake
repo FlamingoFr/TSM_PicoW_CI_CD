7f31b15726be3618581cdf51e0dead412d161e75
franz.schnyder <franz.schnyder@toradex.com>
2024-12-08 13:35:06 +0100

    Initial commit

---
+++ build/debug/srcLib/McuLib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/franzs/Documents/HSLU/TSM_SoftwEng/TSM_PicoW_CI_CD/McuLib
+# Build directory: /home/franzs/Documents/HSLU/TSM_SoftwEng/TSM_PicoW_CI_CD/build/debug/srcLib/McuLib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/debug/srcLib/rdimon/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/franzs/Documents/HSLU/TSM_SoftwEng/TSM_PicoW_CI_CD/McuLib/rdimon
+# Build directory: /home/franzs/Documents/HSLU/TSM_SoftwEng/TSM_PicoW_CI_CD/build/debug/srcLib/rdimon
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
